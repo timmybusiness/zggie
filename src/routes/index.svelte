d7333bc7ab83935578cb8bffb650faa82779fe6f
timmybusiness <46636455+timmybusiness@users.noreply.github.com>
2022-01-13 17:38:52 -0800

    Tablet pressure sensitivity measurer.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+
+    div 
+    { 
+        width: 1000px; height: 1000px;
+    }
+</style>
+
+<script>
+	let m = { x: 0, y: 0, pressure: '' };
+
+    /*  
+        Windows Firefox:    works
+        Windows Chrome:     buggy (swipe left interrupts)
+        Mac Firefox:        works
+        Mac Chrome:         works
+        Mac Safari:         no response
+        iPad Safari:        buggy (scroll interrupts)
+    */
+	function handlePressure(event) {
+		//m.x = event.clientX;
+		//m.y = event.clientY;
+        m.pressure = event.pressure;
+	}
+</script>
+
+<div on:pointermove={handlePressure}>
+	The pressure is {m.pressure}
+</div>
+
+
+
